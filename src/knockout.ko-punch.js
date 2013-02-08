fb003f7588dcfe177f775412077ef122dc5aa5a7
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2013-02-07 21:32:04 -0500

    Fixed prototype inheritance mess mess :)

--- src/knockout.ko-punch.js
+++ src/knockout.ko-punch.js
@@ -1,30 +1,6 @@
 /* ko-punch: knockout.ko-punch.js */
 
 (function (ko,undefined) {
-
-	var _kopunch_;
-
-	/*
-	 * To create an easy-punch ViewModel, inherit from PunchViewModel using prototypal inheritance
-	 * 
-	 * example:
-	 *	
-	 *	function ViewModel () {}
-	 *	ViewModel.prototype = new PunchViewModel();
-	 *	var vm = new ViewModel();
-	 *
-	 * Alternatively, extend either a new PunchModel or the PunchModel constructor either with
-	 * raw javascript or your library's extension method
-	 *
-	 * examples:
-	 *
-	 *	ViewModel.punch = PunchViewModel.punch;
-	 *
-	 *	$.extend(true, ViewModel, new PunchViewModel());
-	 *	$.extend(true, ViewModel, { punch: PunchViewModel.punch });
-	 *
-	 */
-	function PunchViewModel () {}
 	
 	/*
 	 * the punch function creates a new observable on the inheriting ViewModel
@@ -44,48 +20,32 @@
 	 *
 	 */
 
-	PunchViewModel.prototype = {
-		punch: function (observable, value) {
-			if (arguments.length == 0) {
-				return this.punch;
-			}
-			if (typeof(observable) != "string") {
-				value = observable.value;
-				observable = observable.observable;
-			}
-			return _kopunch_.observable.call(this, observable, value);
-		}
-	};
-	PunchViewModel.punch = PunchViewModel.prototype.punch;	
-	_kopunch_ = PunchViewModel.punch.fn = PunchViewModel.punch.prototype = {
-		base: function (type, observable, value) {
+	var kopunch = function (target) {
+		var punched = target;
+
+		var _punch_ = function (observable, value) {
+			_punch_.base.call(punched, "observable", observable, value);
+		};
+		_punch_.base = function (type, observable, value) {
 			if (typeof(observable) !== "string") {
 				value = observable.value;
 				observable = observable.observable;
 			}
-			return ko[type]().punch(this, observable, value);
-		},
-		subscribable: function (observable, value) {
-			return _kopunch_.base.call(this, "subscribable", observable, value);
-		},
-		observable: function (observable, value) {
-			return _kopunch_.base.call(this, "observable", observable, value);
-		},
-		observableArray: function (observable, value) {
-			return _kopunch_.base.call(this, "observableArray", observable, value);
-		}
+			return ko[type]().punch(punched, observable, value);
+		};
+		_punch_.subscribable = function (observable) {
+			return punched[observable] = new ko.subscribable();
+		};
+		_punch_.observable = function (observable, value) {
+			return _punch_.base("observable", observable, value);
+		};
+		_punch_.observableArray = function (observable, value) {
+			return _punch_.base("observableArray", observable, value);
+		};
+
+		return _punch_;
 	};
-	/*
-	PunchViewModel.punch.base = function () { return _kopunch_.base.apply(PunchViewModel, arguments) };
-	PunchViewModel.punch.subscribable = function () { return _kopunch_.subscribable.apply(PunchViewModel, arguments) };
-	PunchViewModel.punch.observable = function () { return _kopunch_.observable.apply(PunchViewModel, arguments) };
-	PunchViewModel.punch.observableArray = function () { return _kopunch_.observableArray.apply(PunchViewModel, arguments) };
-	*/
-	PunchViewModel.punch.base = _kopunch_.base;
-	PunchViewModel.punch.subscribable = _kopunch_.subscribable;
-	PunchViewModel.punch.observable = _kopunch_.observable;
-	PunchViewModel.punch.observableArray = _kopunch_.observableArray;
-	
+
 	/*
 	 * call with either all three params or a params object with target, observable, value properties
 	 * values option is totally optional and it may make more sense to you to put the value into the
@@ -106,6 +66,7 @@
 	 *
 	 */
 	ko.subscribable.fn.punch = function (target,observable,value) {
+		if (!target) return;
 		if (observable == undefined) {
 			observable = target.observable;
 			value = target.value;
@@ -118,8 +79,14 @@
 		return this;
 	};
 
-	// Attach base punch view model to knockout
-	ko.baseModels = ko.baseModels || {};
-	ko.baseModels.PunchViewModel = PunchViewModel;
+	/*
+	 * attach to your ViewModel with the ko.punch method
+	 * 
+	 * example:
+	 *
+	 *	ViewModel.punch = ko.punch(ViewModel);
+	 *
+	 */
+	ko.punch = kopunch;
 
 })(ko);
